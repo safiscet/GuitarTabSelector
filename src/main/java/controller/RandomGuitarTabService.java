da28820a38f1299f4412a5d925731d72d4ab8b3c
Stefan Fritsch <stef.fritsch@gmx.de>
2017-05-31 15:17:54 +0200

    Outputs are now actually random

--- src/main/java/controller/RandomGuitarTabService.java
+++ src/main/java/controller/RandomGuitarTabService.java
@@ -6,6 +6,7 @@ import model.GuitarTabConfiguration;
 import model.NoSuchGuitarTabException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -23,6 +24,7 @@ public class RandomGuitarTabService {
 
     public RandomGuitarTabService(GuitarTabConfiguration config, GuitarTabProvider guitarTabProvider) {
         guitarTabs = new ArrayList<>(guitarTabProvider.getAllGuitarTabs());
+        Collections.shuffle(guitarTabs);
         System.out.println(guitarTabs);
         iterator = guitarTabs.listIterator();
     }

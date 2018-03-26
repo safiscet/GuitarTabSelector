69a802b70d7198fbc5e582faa8d5fcc0279cbe22
Stefan Fritsch <stef.fritsch@gmx.de>
2018-03-26 21:54:41 +0200

    Update gradle-wrapper to version 4.6 and use junit 5

---
+++ GuitarTabSelectorCore/src/test/java/de/safiscet/guitartabselector/GuitarTabUtilsTest.java
@@ -0,0 +1,15 @@
+package de.safiscet.guitartabselector;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Stefan Fritsch on 26.03.2018.
+ */
+class GuitarTabUtilsTest {
+
+    @Test
+    void test() {
+        //TODO: write real test
+    }
+}
